37bf1d0e2a0892a52ba546cb724e7c264db1ef14
Carlos <jose.camacho.s@ucb.edu.bo>
2021-09-15 18:45:24 -0400

    add size memory

--- N-puzzle.py
+++ N-puzzle.py
@@ -1,10 +1,12 @@
+import time
+import  sys
 from heapq import heapify, heappush, heappop;
 
-def hamming (dim, grid, target):
+def tiles_out_of_place(dim, grid, target):
     counter = 0
     for i in range (dim):
         for j in range (dim):
-            if (not (grid [i][j] == target [i][j] or grid [i][j] == 0)):
+            if (grid [i][j] != target [i][j]):
                 counter += 1
     return counter
 
@@ -49,35 +51,119 @@ def getNextStates (dim, current):
         nextStates.append (('DOWN', d, (empty[0] + 1, empty [1])))
     return nextStates
 
-def getSequenceInfo(dim, grid):
+def sum_of_inverse_permutations(dim, grid, target):
     count = 0
+    aux=0
+    vector = []
+    for i in range(dim):
+        for j in range(dim):
+            vector.append(grid[i][j])
+            aux += 1
+    for i in vector:
+        for j in range(i,len(vector)):
+            if(vector[i] > vector[j]):
+                count += 1
+    return count   
+
+def getSequenceInfo(dim, grid):
+    count = -1
     target = [[j for j in range(i, i + dim)] for i in range(0, (dim * (dim - 1)) + 1, dim)]
+    nodeSize = sys.getsizeof(grid)
+    initialTime = time.time()
+    current = (tiles_out_of_place(dim, grid, target), 0, [], grid)
+    stateTree = [current]
+    heapify(stateTree)
+    while(not current [-1] == target):
+        current = heappop(stateTree)
+        for state in getNextStates(dim, current [-1]):
+            heappush(stateTree, (tiles_out_of_place(dim, state[1], target) + current[1] + 1, current[1] + 1, current[2] + [state[0]], state[1]))
+            count += 1
+    finalTime = time.time()
+    print("Total search time elapsed: " + str(finalTime - initialTime)+" seconds")
+    print ("numero de nodos" + str(count))
+    print ("espacio de memoria: "+ str(((nodeSize* count)/1024)/1024)+" MB")
+    return current[1], current[2]
+
+def getSequenceInfo2(dim, grid):
+    count = -1
+    target = [[j for j in range(i, i + dim)] for i in range(0, (dim * (dim - 1)) + 1, dim)]
+    nodeSize = sys.getsizeof(grid)
+    initialTime = time.time()
     current = (manhattan(dim, grid, target), 0, [], grid)
     stateTree = [current]
     heapify(stateTree)
     while(not current [-1] == target):
         current = heappop(stateTree)
-        count += 1
         for state in getNextStates(dim, current [-1]):
             heappush(stateTree, (manhattan(dim, state[1], target) + current[1] + 1, current[1] + 1, current[2] + [state[0]], state[1]))
-
-    print (count)
+            count += 1
+    finalTime = time.time()
+    print("Total search time elapsed: " + str(finalTime - initialTime)+" seconds")
+    print ("numero de nodos" + str(count))
+    print ("espacio de memoria: "+ str(((nodeSize* count)/1024)/1024)+" MB")
     return current[1], current[2]
 
+def getSequenceInfo3(dim, grid):
+    count = -1
+    target = [[j for j in range(i, i + dim)] for i in range(0, (dim * (dim - 1)) + 1, dim)]
+    nodeSize = sys.getsizeof(grid)
+    initialTime = time.time()
+    current = (sum_of_inverse_permutations(dim, grid, target), 0, [], grid)
+    stateTree = [current]
+    heapify(stateTree)
+    while(not current [-1] == target):
+        current = heappop(stateTree)
+        for state in getNextStates(dim, current [-1]):
+            heappush(stateTree, (sum_of_inverse_permutations(dim, state[1], target) + current[1] + 1, current[1] + 1, current[2] + [state[0]], state[1]))
+            count += 1
+    finalTime = time.time()
+    print("Total search time elapsed: " + str(finalTime - initialTime)+" seconds")
+    print ("numero de nodos" + str(count))
+    print ("espacio de memoria: "+ str(((nodeSize* count)/1024)/1024)+" MB")
+    return current[1], current[2]
 
 if (__name__ == '__main__'):
-	dim = int (input ())
-	row = numCount = 0
-	grid = [[] for i in range (dim)]
+    print("Enter the size of the puzzle: ")
+    dim = int (input ())
+    row = numCount = 0
+    print("Enter the elements: ")
+    grid = [[] for i in range (dim)]
 
-	for i in range (dim ** 2):
-		grid [row].append (int (input ()))
-		numCount += 1
-
-		if (numCount % dim == 0):
-			row += 1
-			numCount = 0
-
-	seqCount, sequence = getSequenceInfo (dim, grid)
-	print (seqCount)
-	#print ('\n'.join (sequence))
+    for i in range (dim ** 2):
+        grid [row].append (int (input ()))
+        numCount += 1
+        if (numCount % dim == 0):
+            row += 1
+            numCount = 0
+    option = 0
+    while(option != 5):
+        print("1. Use the number of tiles out of place")
+        print("2. Use the Manhattan distance")
+        print("3. Use the sum of inverse permutations")
+        print("4. Use all the heuristics")
+        option = input()
+        if option == '1':
+            print("- Tiles out of place -")
+            seqCount, sequence = getSequenceInfo (dim, grid)
+            print (seqCount)
+            print()
+        if option == '2':
+            print("- Manhattan Distance -")
+            seqCount, sequence = getSequenceInfo2 (dim, grid)
+            print (seqCount)
+            print()
+        if option == '3':
+            print("- Sum of inverse permutations - ")
+            seqCount, sequence = getSequenceInfo3 (dim, grid)
+            print (seqCount)
+            print()
+        if option == '4':
+            print("- Tiles out of place -")
+            seqCount, sequence = getSequenceInfo (dim, grid)
+            print (seqCount)
+            print("- Manhattan Distance -")
+            seqCount, sequence = getSequenceInfo2 (dim, grid)
+            print (seqCount)
+            print("- Sum of inverse permutations - ")
+            seqCount, sequence = getSequenceInfo3 (dim, grid)
+            print ("step: "+ str(seqCount))
